9c99f1e0c032554cf8a67339d636125f334cb36b
Wim <w.j.stienstra@home.nl>
2020-12-08 12:03:27 +0100

    Initial commit

---
+++ App/Waterplantinator_App/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Waterplantinator_App
+{
+	public partial class MainPage : ContentPage
+	{
+		public ObservableCollection<SensorData> sensorData { get; private set; }
+
+		public MainPage()
+		{
+			InitializeComponent();
+			BindingContext = this;
+
+			sensorData = new ObservableCollection<SensorData>();
+
+			SensorData data = new SensorData(60, 70, 23, 70, 20, DateTime.Now);
+			sensorData.Add(data);
+		}
+	}
+}
---
+++ App/Waterplantinator_App/SensorData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Waterplantinator_App
+{
+	public class SensorData
+	{
+		public int Humidity { get; set; }
+		public int Light { get; set; }
+		public int Temperature { get; set; }
+		public int Watertank { get; set; }
+		public int Soilmoist { get; set; }
+		public DateTime Time { get; set; }
+
+		public SensorData(int humidity, int light, int temperature, int watertank, int soilmoist, DateTime time)
+		{
+			this.Humidity = humidity;
+			this.Light = light;
+			this.Temperature = temperature;
+			this.Watertank = watertank;
+			this.Soilmoist = soilmoist;
+			this.Time = time;
+		}
+
+	}
+}
