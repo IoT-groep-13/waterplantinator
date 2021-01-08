a6fa60b71aaf7917f5320203fe6283c4650e3b1c
Mariska <twosidedshadows@gmail.com>
2021-01-08 14:46:58 +0100

    IP-adres and IP-poort + Toggle for Light/Dark mode

--- App/Waterplantinator_App/Settings.xaml.cs
+++ App/Waterplantinator_App/Settings.xaml.cs
@@ -18,23 +18,38 @@ namespace Waterplantinator_App
 			FileInfo fi = new FileInfo(asm.Location);
 
 			VersionLabel.Text = fi.LastWriteTime.ToShortDateString() + " V" + VersionTracking.CurrentVersion;
+			ThemeSwitch.IsToggled = Application.Current.RequestedTheme == OSAppTheme.Dark;
+			Client.ip = Preferences.Get("IP-adres", "192.168.0.45");
+			ipEntry.Text = Client.ip;
+			Client.port = Preferences.Get("IP-poort", 11000);
+			ipPortEntry.Text = Client.port.ToString();
 		}
-
-        private void LightButton_Clicked(object sender, EventArgs e)
-        {
-			Application.Current.UserAppTheme = OSAppTheme.Light;
-			Preferences.Set("theme", "Light");
-		}
-
-		private void DarkButton_Clicked(object sender, EventArgs e)
-		{
-			Application.Current.UserAppTheme = OSAppTheme.Dark;
-			Preferences.Set("theme", "Dark");
-		}
-
+		     
 		private void BackButton_OnClicked(object sender, EventArgs e)
 		{
 			Navigation.PopModalAsync();
 		}
-	}
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)		
+        {
+			if(e.Value)
+            {
+				Application.Current.UserAppTheme = OSAppTheme.Dark;
+				Preferences.Set("theme", "Dark");				
+			}
+			else
+            {
+				Application.Current.UserAppTheme = OSAppTheme.Light;
+				Preferences.Set("theme", "Light");
+			}
+		}
+        private void SubmitButton_Clicked(object sender, EventArgs e)
+        {
+			Client.ip = ipEntry.Text;
+			Preferences.Set("IP-adres", Client.ip);
+			Client.port = Convert.ToInt32(ipPortEntry.Text);
+			Preferences.Set("IP-poort", Client.port);
+			DisplayAlert("Gegevens opgeslagen", "Uw gegevens zijn opgeslagen!", "OK");
+        }
+    }
 }
